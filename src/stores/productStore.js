9422a429b2c29ef30fa7d21e6ba31c68a36c9c17
Nandaaung-Dev <112558688+Nandaaung-Dev@users.noreply.github.com>
2025-01-15 21:44:14 +0630

    pass by value with shallow copy

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,12 +1,13 @@
 import { defineStore } from "pinia";
-import products from '@/data/products.json'
-export const useProductStore = defineStore('product', {
-    state: () => ({
-        products
-    }),
-    actions: {
-        addProduct(product) {
-            this.products.push(product);
-        },
+import products from "@/data/products.json";
+export const useProductStore = defineStore("product", {
+  state: () => ({
+    products,
+  }),
+  actions: {
+    addProduct(product) {
+      const productCopy = { ...product };
+      this.products.push(productCopy);
     },
- });
+  },
+});

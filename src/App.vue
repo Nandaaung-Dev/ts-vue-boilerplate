c58396689adc57707452d23d0a46d04e3406aa3e
Nandaaung-Dev <112558688+Nandaaung-Dev@users.noreply.github.com>
2025-01-22 23:08:30 +0630

    update:

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <input type="text" v-model="user">
-    <pre @click="showAlert(user)">{{ user }}-{{ usernameOfLength }}</pre>
+    <!-- <input type="text" v-model="user">
+    <pre @click="showAlert(user)">{{ user }}-{{ usernameOfLength }}</pre> -->
+    <RouterView></RouterView>
   </div>
 </template>
 
 <script>
 import { useAuthUserStore } from "@/stores/authUserStore"
 import { mapWritableState, mapActions, mapState } from "pinia";
+import { RouterView } from "vue-router";
 export default {
   computed: {
     ...mapWritableState(useAuthUserStore, {
---
+++ src/views/Playground.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <pre>{{ data.count }}</pre>
+        <button @click="add">+</button>
+    </div>
+</template>
+
+<script setup>
+import { reactive, watch } from 'vue';
+
+
+
+const data = reactive({ count: 0 })
+
+
+const add = () => {
+    return data.count++
+}
+
+// this won't work because we are passing a number to watch()
+watch(data.count, (count) => {
+  console.log(`Count is: ${count}`)
+})
+
+// instead, use a getter:
+watch(
+  () => data.count,
+  (count) => {
+    console.log(`Count is: ${count} with getter`)
+  }
+)
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
